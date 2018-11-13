851b01ae06fffe2931e50804c9c275879c58c31c
Sabine <10sloe@itg-dah.de>
2018-11-13 18:19:31 +0100

    Gemeinsames Projekt angelegt

---
+++ Controller.java
@@ -0,0 +1,75 @@
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import java.text.NumberFormat;
+import javafx.event.ActionEvent;
+
+
+class Controller {
+
+    Dreieck dreieck;
+
+    @FXML
+    private TextField txtA;
+
+    @FXML
+    private TextField txtC;
+
+    @FXML
+    private Label lblErg;
+
+    @FXML
+    void initialize() {
+        dreieck = new Dreieck();
+
+    }
+
+    @FXML
+    void berechne_a_c(ActionEvent event) {
+        try{
+            dreieck.setA(Double.parseDouble(txtA.getText()));
+            dreieck.setC(Double.parseDouble(txtC.getText()));
+
+            double erg = dreieck.berechne_A_C();
+            NumberFormat n = NumberFormat.getInstance();
+            n.setMaximumFractionDigits(2);
+
+            lblErg.setText("Die Steigung der Geraden AC beträgt "+ n.format(erg));
+        } catch(Exception ex){}
+    }
+
+    @FXML
+    void berechne_alpha(ActionEvent event) {
+        try{
+            dreieck.setA(Double.parseDouble(txtA.getText()));
+            dreieck.setC(Double.parseDouble(txtC.getText()));
+
+            double erg = dreieck.berechneAlpha();
+            NumberFormat n = NumberFormat.getInstance();
+            n.setMaximumFractionDigits(2);
+
+            lblErg.setText("Der Winkel alpha beträgt "+ n.format(erg) + "°");
+        } catch(Exception ex){
+        }
+
+    }
+
+    @FXML
+    void berechneb(ActionEvent event) {
+        try{
+            dreieck.setA(Double.parseDouble(txtA.getText()));
+            dreieck.setC(Double.parseDouble(txtC.getText()));
+
+            double erg = dreieck.berechneB();
+            NumberFormat n = NumberFormat.getInstance();
+            n.setMaximumFractionDigits(2);
+
+            lblErg.setText("Die Länge der Hypotenuse beträgt "+ n.format(erg));
+        }
+        catch(Exception ex)
+        {}
+    }
+
+}
---
+++ Dreieck.java
@@ -0,0 +1,40 @@
+
+class Dreieck
+{
+    double a;
+    double c;
+    
+    Dreieck()
+    {
+        a = 5;
+        c = 3;
+    }
+
+    void setA(double a_)
+    {
+        a = a_;
+    }
+
+    void setC(double c_)
+    {
+        c = c_;
+    }
+
+    double berechneB()
+    {
+      
+        return 0;
+    }
+
+    double berechne_A_C()
+    {
+        return 0;
+    }
+    
+    double berechneAlpha()
+    {
+        
+        return 0;
+    }
+
+}
